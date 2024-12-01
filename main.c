b566c6188f6aeb727209d053443ce77759d447bd
Silas.nguyen <nguyentung23092003@gmail.com>
2024-12-02 00:38:19 +0700

    update main.c

--- main.c
+++ main.c
@@ -5,24 +5,41 @@
 #include "fcntl.h"
 #include "sys/stat.h"
 #include "sys/types.h"
+#include "string.h"
 
-#define FIFO_NAME "myfifo"
+#define FIFO_NAME "./myfifo"
+#define BUFF_SIZE 100
 
 pid_t pid;
 pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER; // khai bao khoa mutex lock dam bao su dung shared resource
-//pthread_cond_t cond = PTHREAD_COND_INITIALIZER; // khai bao cond
+pthread_cond_t cond = PTHREAD_COND_INITIALIZER; // khai bao cond
 
 typedef struct {
     char name[30];
     char msg[30];
+    char msg_connection[40];
     float temperature;
 } shared_data;
 
 static void *connection_handle(void *args)
-{
+{   
+    shared_data *thr = (shared_data *)args;
+    char buff[BUFF_SIZE] = "Connecting....";
     pthread_detach(pthread_self());
     pthread_mutex_lock(&lock);
+    int fd = open(FIFO_NAME,O_WRONLY); // mo file fifo >> gan gia tri mo file = fd o parentprocess
+        if(fd < 0){
+            printf("Failed to open FIFO for writting.");
+            exit(1);
+        }
     printf("connection manager.\n");
+    strncpy(thr->msg_connection,"Connecting.....",sizeof(thr->msg_connection));
+    printf("%s\n",thr->msg_connection);
+    write(fd, buff, strlen(buff) +1);
+    sleep(3);
+    strncpy(thr->msg_connection,"Connected",sizeof(thr->msg_connection));
+    printf("%s\n",thr->msg_connection);
+    close(fd);
     pthread_mutex_unlock(&lock);
     pthread_exit(NULL);
 
@@ -66,24 +83,25 @@ int main(int argc,char const *argv[])
     }
     else if(pid == 0)
     {   
-        printf("Im child process. %d\n",getpid());
-        int fd = open(FIFO_NAME, O_RDONLY);
-        if(fd < 0){
+        int fd;
+        char msg[100] = {0}; // fd child_process
+        printf("Im child process. %d\n",getpid());   
+        if(fd = open(FIFO_NAME, O_RDONLY) < 0){
             printf("Failed to open FIFO for reading.");
             exit(1);
-        } 
+        }
+        read(fd, msg, BUFF_SIZE);
+        printf("msg: %s\n", msg);
+        close(fd);
     }
     else{
         printf("Iam parent process. %d\n",getppid());
         pthread_t connection, data, storage_mng; // khai bao cac thread o req.2 
-        shared_data shd;
-        int fd = open(FIFO_NAME,O_WRONLY); // mo file fifo >> gan gia tri mo file = fd 
-        if(fd < 0){
-            printf("Failed to open FIFO for reading.");
-            exit(1);
-        } 
+        shared_data shd; 
         pthread_create(&connection,NULL,&connection_handle,&shd);
         pthread_create(&data,NULL,&data_handle,&shd);
-        pthread_create(&storage_mng,NULL,&storage_mng_handle,&shd);           
+        pthread_create(&storage_mng,NULL,&storage_mng_handle,&shd);
+        while(1)
+        {}           
     }
 }
